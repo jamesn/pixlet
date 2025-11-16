ec0ff190dc97cb30add12e476556249015910e99
James Nuckolls <jamesn@what.net>
2025-11-16 15:04:22 -0600

    Improve the bundle size

--- src/features/schema/FieldDetails.jsx
+++ src/features/schema/FieldDetails.jsx
@@ -1,42 +1,47 @@
-import React from 'react';
-
-import OAuth2 from './fields/oauth2/OAuth2';
-import PhotoSelect from './fields/photoselect/PhotoSelect';
-import RawPhotoSelect from './fields/photoselect/RawPhotoSelect';
-import Toggle from './fields/Toggle';
-import Color from './fields/Color';
-import DateTime from './fields/DateTime';
-import Dropdown from './fields/Dropdown';
-import LocationBased from './fields/location/LocationBased';
-import LocationForm from './fields/location/LocationForm';
-import TextInput from './fields/TextInput';
-import Typeahead from './fields/Typeahead';
+import React, { Suspense } from 'react';
 import Typography from '@mui/material/Typography';
 
+const OAuth2 = React.lazy(() => import('./fields/oauth2/OAuth2'));
+const PhotoSelect = React.lazy(() => import('./fields/photoselect/PhotoSelect'));
+const RawPhotoSelect = React.lazy(() => import('./fields/photoselect/RawPhotoSelect'));
+const Toggle = React.lazy(() => import('./fields/Toggle'));
+const Color = React.lazy(() => import('./fields/Color'));
+const DateTime = React.lazy(() => import('./fields/DateTime'));
+const Dropdown = React.lazy(() => import('./fields/Dropdown'));
+const LocationBased = React.lazy(() => import('./fields/location/LocationBased'));
+const LocationForm = React.lazy(() => import('./fields/location/LocationForm'));
+const TextInput = React.lazy(() => import('./fields/TextInput'));
+const Typeahead = React.lazy(() => import('./fields/Typeahead'));
 
 export default function FieldDetails({ field }) {
-    switch (field.type) {
-        case 'datetime':
-            return <DateTime field={field} />
-        case 'dropdown':
-            return <Dropdown field={field} />
-        case 'location':
-            return <LocationForm field={field} />
-        case 'locationbased':
-            return <LocationBased field={field} />
-        case 'oauth2':
-            return <OAuth2 field={field} />
-        case 'png':
-            return <PhotoSelect field={field} />
-        case 'text':
-            return <TextInput field={field} />
-        case 'onoff':
-            return <Toggle field={field} />
-        case 'typeahead':
-            return <Typeahead field={field} />
-        case 'color':
-            return <Color field={field} />
-        default:
-            return <Typography>Unsupported type: {field.type}</Typography>
-    }
+    return (
+        <Suspense fallback={<div />}> 
+            {(() => {
+                switch (field.type) {
+                    case 'datetime':
+                        return <DateTime field={field} />
+                    case 'dropdown':
+                        return <Dropdown field={field} />
+                    case 'location':
+                        return <LocationForm field={field} />
+                    case 'locationbased':
+                        return <LocationBased field={field} />
+                    case 'oauth2':
+                        return <OAuth2 field={field} />
+                    case 'png':
+                        return <PhotoSelect field={field} />
+                    case 'text':
+                        return <TextInput field={field} />
+                    case 'onoff':
+                        return <Toggle field={field} />
+                    case 'typeahead':
+                        return <Typeahead field={field} />
+                    case 'color':
+                        return <Color field={field} />
+                    default:
+                        return <Typography>Unsupported type: {field.type}</Typography>
+                }
+            })()}
+        </Suspense>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Main from './Main';
-import OAuth2Handler from './features/schema/fields/oauth2/OAuth2Handler';
+const Main = React.lazy(() => import('./Main'));
+const OAuth2Handler = React.lazy(() => import('./features/schema/fields/oauth2/OAuth2Handler'));
 import store from './store';
-import DevToolsTheme from './features/theme/DevToolsTheme';
+const DevToolsTheme = React.lazy(() => import('./features/theme/DevToolsTheme'));
 
 const App = () => {
     return (
         <Provider store={store}>
-            <DevToolsTheme>
-                <BrowserRouter>
-                    <Routes>
-                        <Route exact path="/" element={<Main />} />
-                        <Route path="oauth-callback" element={<OAuth2Handler />} />
-                    </Routes>
-                </BrowserRouter>
-            </DevToolsTheme>
+            <React.Suspense fallback={<div />}> 
+                <DevToolsTheme>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route exact path="/" element={<Main />} />
+                            <Route path="oauth-callback" element={<OAuth2Handler />} />
+                        </Routes>
+                    </BrowserRouter>
+                </DevToolsTheme>
+            </React.Suspense>
         </Provider >
     )
 }
